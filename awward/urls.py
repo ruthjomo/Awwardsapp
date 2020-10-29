5a025f06f5d4cd15fca57f18e98b04fbea3d4d41
ruthjomo <ruthjomo19@gmail.com>
2020-10-29 11:42:15 +0300

    added links

--- awward/urls.py
+++ awward/urls.py
@@ -20,4 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('',include('awwardapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
